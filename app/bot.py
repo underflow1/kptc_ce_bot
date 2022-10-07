f5437bd40ef3c6cc8db1c3d110f5c275d7402e18
user <syncamide@gmail.com>
2022-10-07 19:13:33 +0300

    linter

--- app/bot.py
+++ app/bot.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, Update, KeyboardButton
+from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     Updater,
     CommandHandler,
@@ -15,24 +15,24 @@ load_dotenv()
 
 # Ведение журнала логов
 logging.basicConfig(
-    format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(funcName)s - %(message)s', level=logging.INFO
+    format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(funcName)s - %(message)s',
+    level=logging.INFO
 )
-# FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
 
 logger = logging.getLogger(__name__)
 
-
 LOCATIONS = ['Дубнинская', 'Никитский', 'СКО', 'Тиснум']
 keyboard_locations_items = []
+
 for location in LOCATIONS:
-    keyboard_locations_items.append(InlineKeyboardButton("▫️" + location, callback_data=location))
-keyboard_locations_items.append(InlineKeyboardButton("❌ Отменить", callback_data="end"))
+    keyboard_locations_items.append(
+        InlineKeyboardButton("▫️" + location, callback_data=location)
+    )
 
-keyboard_cancel_item = [InlineKeyboardButton("❌ Отменить", callback_data="end")]
+keyboard_cancel_item = InlineKeyboardButton("❌ Отменить", callback_data="end")
+keyboard_locations_items.append(keyboard_cancel_item)
 
-
-CHOOSE_LOCATION_STAGE = 'choose_location_stage'
-ADD_PHOTO_STAGE = 'add_photo_stage'
+CHOOSE_LOCATION_STAGE, ADD_PHOTO_STAGE = range(2)
 
 
 def build_menu(buttons, n_cols,
@@ -45,18 +45,20 @@ def build_menu(buttons, n_cols,
         menu.append([footer_buttons])
     return menu
 
+
 def get_message_id(message):
     return {'chat_id': message.chat.id, 'message_id': message.message_id}
 
+
 def start(update, context):
     """Вызывается по команде `/start`."""
-
     user = update.message.from_user
     context.user_data['username'] = user.first_name
     logger.info(f"Пользователь {context.user_data['username']} начал разговор")
 
-    keyboard = [keyboard_locations_items]
-    reply_markup = InlineKeyboardMarkup(build_menu(keyboard_locations_items, n_cols=1))
+    reply_markup = InlineKeyboardMarkup(
+        build_menu(keyboard_locations_items, n_cols=1)
+    )
 
     message = update.message.reply_text(
         text="Выберите объект", reply_markup=reply_markup
@@ -78,12 +80,14 @@ def location_choosed(update, context):
     context.user_data['location'] = query.data
     logger.info(f"Пользователь {context.user_data['username']} локация {context.user_data['location']}")
 
-
-    keyboard = [keyboard_cancel_item]
+    keyboard = [[keyboard_cancel_item]]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     cancel_choose_message = context.user_data['cancel_choose_message']
-    bot.delete_message(chat_id=cancel_choose_message['chat_id'], message_id=cancel_choose_message['message_id'])
+    bot.delete_message(
+        chat_id=cancel_choose_message['chat_id'],
+        message_id=cancel_choose_message['message_id']
+    )
 
     location_message_text = f"▪️ выбран объект {query.data}"
     message = bot.send_message(
@@ -100,54 +104,76 @@ def location_choosed(update, context):
 
 
 def repeat_photo(update, context):
-    logger.info(f"Пользователь {context.user_data['username']} локация {context.user_data['location']}")
+    logger.info(
+        f"Пользователь {context.user_data['username']} \
+        локация {context.user_data['location']}"
+    )
 
     query = update.callback_query
     query.answer()
 
-    keyboard = [keyboard_cancel_item]
+    keyboard = [[keyboard_cancel_item]]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     query.edit_message_text(
-        text=f"Добавляйте фото", reply_markup=reply_markup
+        text="Добавляйте фото", reply_markup=reply_markup
     )
 
     return ADD_PHOTO_STAGE
 
 
+def no_location(update, context):
+    if context.user_data['added_count'] == 0:
+        bot.delete_message(
+            chat_id=update.message.chat.id,
+            message_id=update.message.message_id
+        )
+        update.message.reply_text(
+            text="❌ НЕ ВЫБРАН ОБЪЕКТ ❌",
+        )
+    return CHOOSE_LOCATION_STAGE
+
+
 def photo(update, context):
+    """Stores the photo and asks for a location."""
     if context.user_data['location_message']:
         message = context.user_data['location_message']
-        bot.edit_message_text(chat_id=message['chat_id'], message_id=message['message_id'], text=message['text'])
+        bot.edit_message_text(
+            chat_id=message['chat_id'],
+            message_id=message['message_id'],
+            text=message['text']
+        )
         context.user_data['location_message'] = False
 
     if context.user_data['cancel_photo_message']:
         message = context.user_data['cancel_photo_message']
-        bot.delete_message(chat_id=message['chat_id'], message_id=message['message_id'])
+        bot.delete_message(
+            chat_id=message['chat_id'],
+            message_id=message['message_id']
+        )
 
     context.user_data['added_count'] = context.user_data['added_count'] + 1
 
-    logger.info(f"Пользователь {context.user_data['username']} \
-локация {context.user_data['location']} \
-добавлено фото {context.user_data['added_count']}")
+    logger.info(
+        f"Пользователь {context.user_data['username']} локация {context.user_data['location']} добавлено фото {context.user_data['added_count']}"
+    )
 
-
-    """Stores the photo and asks for a location."""
-    user = update.message.from_user
-    # print(update.message.photo)
     photo_file = update.message.photo[-1].get_file()
     photo_file.download("user_photo.jpg")
-    # logger.info("Photo of %s: %s", user.first_name, "user_photo.jpg")
-
 
     keyboard = [
         [
-            InlineKeyboardButton("✔️ Добавить еще!", callback_data="repeat_photo"),
+            InlineKeyboardButton(
+                "✔️ Добавить еще!", callback_data="repeat_photo"
+            ),
             InlineKeyboardButton("❌ Закончить", callback_data="end"),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    message = update.message.reply_text(text=f"▪️ добавлено {context.user_data['added_count']} фото.", reply_markup=reply_markup)
+    message = update.message.reply_text(
+        text=f"▪️ добавлено {context.user_data['added_count']} фото.",
+        reply_markup=reply_markup
+    )
     cancel_photo_message = get_message_id(message)
     context.user_data['cancel_photo_message'] = cancel_photo_message
 
@@ -155,9 +181,13 @@ def photo(update, context):
 
 
 def end(update, context):
-    logger.info(f"Пользователь {context.user_data['username']} локация {context.user_data['location']}")
     """Возвращает `ConversationHandler.END`, который говорит
     `ConversationHandler` что разговор окончен"""
+
+    logger.info(
+        f"Пользователь {context.user_data['username']} локация {context.user_data['location']}"
+    )
+
     query = update.callback_query
     query.answer()
 
@@ -166,20 +196,19 @@ def end(update, context):
         message_id=query.message.message_id,
         reply_markup=None
     )
-    # query.edit_message_text(text="Добавление завершено")
 
     bot.send_message(
         chat_id=query.message.chat.id,
-        text="Чтобы повторить нажмите \n/start",
+        text="Добавление завершено. \nЧтобы повторить нажмите \n/start",
     )
 
     return ConversationHandler.END
 
 
 if __name__ == "__main__":
-
-    bot = Bot(os.getenv("TOKEN"))
-    updater = Updater(os.getenv("TOKEN"))
+    TOKEN = os.getenv("TOKEN")
+    bot = Bot(TOKEN)
+    updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
 
     conv_handler = ConversationHandler(
@@ -187,10 +216,11 @@ if __name__ == "__main__":
             CommandHandler('start', start),
             MessageHandler(Filters.text, start)
         ],
-        states={ # словарь состояний разговора, возвращаемых callback функциями
+        states={
             CHOOSE_LOCATION_STAGE: [
                 CallbackQueryHandler(end, pattern='^end$'),
                 CallbackQueryHandler(location_choosed, pattern='^\w+$'),
+                MessageHandler(Filters.photo, no_location),
             ],
             ADD_PHOTO_STAGE: [
                 MessageHandler(Filters.photo, photo),
